c83326af64d737fd293c2e74868ce7b7e97eaff9
Valeriy <java@yirelav.ru>
2022-11-29 20:44:44 +0100

    feat: worker threads implementation

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,26 @@
+import {Worker} from "node:worker_threads";
+import {cpus} from "os";
+import path from 'path';
+
+
 const performCalculations = async () => {
-    // Write your code here
-};
+    const workerPath = path.resolve('.\\worker.js');
+    const promises = [];
+
+    for (let i = 0; i < cpus().length; i++) {
+        promises.push(new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: i + 10
+            });
+            worker.on('message', resolve);
+            worker.on('error', reject);
+            worker.on('exit', (code) => {
+                if (code !== 0)
+                    reject(new Error(`Worker stopped with exit code ${code}`));
+            });
+        }));
+    }
+    Promise.all(promises).then((values) => console.log(values));
+}
 
 await performCalculations();
