7827dc37f459bdafb25899005ef50598c7bb0d34
Valeriy <java@yirelav.ru>
2022-11-29 18:02:40 +0100

    feat: read.js impl

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import path from 'path';
+import {createReadStream} from "node:fs";
+
 const read = async () => {
-    // Write your code here 
+    const srcPath = path.resolve('.\\files\\fileToRead.txt');
+    const readStream = createReadStream(srcPath, 'utf-8');
+    readStream.on('data', chunk => {
+        console.log(chunk);
+    })
 };
 
 await read();
