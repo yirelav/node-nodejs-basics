88f5a4a9b1fc8038308e556a644c51d7e11a9a48
Valeriy <java@yirelav.ru>
2022-11-29 03:08:37 +0100

    feat: create.js impl

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,22 @@
+import {writeFile, access, constants} from 'node:fs/promises';
+import path from 'path';
+
+
 const create = async () => {
-    // Write your code here 
+
+    let src = path.resolve('.\\files\\fresh.txt');
+
+    try {
+        await access(src, constants.F_OK);
+        console.error('FS operation failed');
+    } catch (err) {
+        try {
+            const data = 'I am fresh and young';
+            await writeFile(src, data);
+        } catch (err) {
+            console.error('FS operation failed');
+        }
+    }
 };
 
 await create();
