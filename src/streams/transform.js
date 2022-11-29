9b23312b7d9fe4c54d67df2796ae6db41c43d6df
Valeriy <java@yirelav.ru>
2022-11-29 18:24:17 +0100

    feat: transform.js impl

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,16 @@
+import {Transform} from "stream";
+
+
 const transform = async () => {
-    // Write your code here 
+    process.stdin.pipe(reverseTransform).pipe(process.stdout);
 };
+
+const reverseTransform = new Transform({
+    transform: (chunk, encoding, done) => {
+        const newLineSymbol = "\n"; // for pretty output
+        const result = chunk.reverse().toString().trim() + newLineSymbol;
+        done(null, result)
+    }
+})
 
 await transform();
