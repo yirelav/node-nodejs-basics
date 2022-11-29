3f2a9601046a500ce76d8c494db2cadc5e19b644
Valeriy <java@yirelav.ru>
2022-11-29 04:43:27 +0100

    feat: calcHash.js impl

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,20 @@
+import path from 'path';
+import {createReadStream} from 'node:fs';
+
+const {createHash} = await import('crypto');
+
 const calculateHash = async () => {
-    // Write your code here 
+    let srcPath = path.resolve('.\\files\\fileToCalculateHashFor.txt');
+    const hash = createHash('sha256');
+    const input = createReadStream(srcPath);
+    input.on('readable', () => {
+        const data = input.read();
+        if (data)
+            hash.update(data);
+        else {
+            console.log(hash.digest('hex'));
+        }
+    });
 };
 
 await calculateHash();
