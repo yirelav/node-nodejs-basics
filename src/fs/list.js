b1006b24450a0c902023740cb558121c98aac936
Valeriy <java@yirelav.ru>
2022-11-29 03:35:57 +0100

    feat: list.js impl

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import {readdir} from 'node:fs/promises';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    let srcPath = path.resolve('.\\files');
+    try {
+        await readdir(srcPath).then(names => console.log(names));
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await list();
