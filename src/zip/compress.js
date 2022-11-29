d8f121df23acc43497dbaf34b2bfd50a9bde90f0
Valeriy <java@yirelav.ru>
2022-11-29 18:57:33 +0100

    feat: compress.js and decompress.js impl

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from "fs";
+import zlib from "zlib";
+import path from 'path';
+
 const compress = async () => {
-    // Write your code here 
+
+    let srcPath = path.resolve('.\\files\\fileToCompress.txt');
+    let dstPath = path.resolve('.\\files\\archive.gz');
+
+    const input = fs.createReadStream(srcPath, 'utf-8');
+    const output = fs.createWriteStream(dstPath);
+    const gzCompressor = zlib.createGzip();
+
+    input.pipe(gzCompressor).pipe(output);
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,16 @@
+import path from "path";
+import fs from "fs";
+import zlib from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    let srcPath = path.resolve('.\\files\\archive.gz');
+    let dstPath = path.resolve('.\\files\\fileToCompress.txt');
+
+    const input = fs.createReadStream(srcPath);
+    const output = fs.createWriteStream(dstPath);
+    const gzDecompressor = zlib.createGunzip();
+
+    input.pipe(gzDecompressor).pipe(output);
 };
 
 await decompress();
