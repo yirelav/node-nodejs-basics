4b15ccc273cd82824b1df2da026c5c7231c6b922
Valeriy <java@yirelav.ru>
2022-11-29 03:26:22 +0100

    feat: rename.js impl

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+import Path from "path";
+import {access, constants} from "node:fs/promises";
+
+
 const rename = async () => {
-    // Write your code here 
+    let srcPath = Path.resolve('.\\files\\wrongFilename.txt');
+    let destPath = Path.resolve('.\\files\\properFilename.md');
+
+    try {
+        await access(destPath, constants.F_OK);
+        console.error('FS operation failed');
+        return;
+    } catch (e) {
+        //ignored
+    }
+
+    try {
+        await fs.rename(srcPath, destPath);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await rename();
