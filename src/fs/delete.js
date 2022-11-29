f6d142cc72ce02d518fcdbdbb1ec87fa91ef6a49
Valeriy <java@yirelav.ru>
2022-11-29 03:28:48 +0100

    feat: delete.js impl

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,13 @@
+import {rm} from "node:fs/promises";
+import path from 'path';
+
 const remove = async () => {
-    // Write your code here 
+    let src = path.resolve('.\\files\\fileToRemove.txt');
+    try {
+        await rm(src);
+    } catch (err) {
+        console.error('FS operation failed');
+    }
 };
 
 await remove();
