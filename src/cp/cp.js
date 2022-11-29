a6cd819debe570769dcd75defdfd1075871b05a1
Valeriy <java@yirelav.ru>
2022-11-29 20:57:52 +0100

    feat: cp.js implementation

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,10 @@
+import child_process from "child_process";
+import path from 'path';
+
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const modulePath = path.resolve('.\\files\\script.js');
+    child_process.fork(modulePath, args);
 };
 
 spawnChildProcess();
